a3421f3a485db7857343f59e41e0e27a450cb360
ParkYoungWoong <thesecon@gmail.com>
2026-08-26 15:15:50 +0900

    260826-15:15

---
+++ src/backups/요소_참조.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useTemplateRef, onMounted } from 'vue'
+
+const inputRef = useTemplateRef('abc')
+
+onMounted(function () {
+  inputRef.value?.focus()
+})
+</script>
+
+<template>
+  <div>
+    <input ref="abc" type="text">
+    <button>검색</button>
+  </div>
+</template>
+
+<style scoped></style>
