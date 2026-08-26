d40f542b6e7c48c4adfe1d3942c73d6d2b3b3fff
ParkYoungWoong <thesecon@gmail.com>
2026-08-26 12:51:55 +0900

    260826-12:52

---
+++ src/backups/반응성_및_속성바인딩.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+// let count = 0
+const colorName = ref('color-red')
+const count = ref(0)
+// const isActive = ref(false)
+// const elName = ref('paragraph')
+const myAttributes = {
+  class: 'active',
+  id: 'paragraph',
+  'data-count': 0
+}
+
+function increase() {
+  count.value += 1
+  console.log(count.value)
+}
+function changeColor() {
+  if (colorName.value === 'color-red') {
+    colorName.value = 'color-blue'
+  } else {
+    colorName.value = 'color-red'
+  }
+}
+</script>
+
+<template>
+  <h1
+    :class="colorName"
+    @click="increase(), changeColor()">
+    Hello {{ count }}~!!
+  </h1>
+  <p
+    v-bind="myAttributes">
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped>
+.color-red {
+  color: red;
+}
+.color-blue {
+  color: blue;
+}
+</style>
