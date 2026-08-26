7eba7e4e198a8c60173a1fd522aa2987688dd7ec
ParkYoungWoong <thesecon@gmail.com>
2026-08-26 14:51:55 +0900

    260826-14:51

---
+++ src/backups/인라인_스타일_바인딩.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const fontSize = ref(16)
+</script>
+
+<template>
+  <!-- <h1 style="font-size: 16px;" class="color-red">Hello world!</h1> -->
+  <h1 :style="{
+    fontSize: fontSize + 'px'
+    // fontSize: `${fontSize}px`
+  }" class="color-red">Hello world!</h1>
+  <button @click="fontSize += 2">크기 증가</button>
+  <button @click="fontSize -= 2">크기 감소</button>
+</template>
+
+<style scoped>
+.color-red {
+  color: red
+  /* font-size: 16px; */
+}
+</style>
---
+++ src/backups/클래스_바인딩.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isActive = ref(false)
+const isNormal = ref(false)
+
+function toggleActive() {
+  isActive.value = !isActive.value
+}
+function toggleWeight() {
+  isNormal.value = !isNormal.value
+}
+</script>
+
+<template>
+  <!-- <h1 class="active font-normal">Hello world!</h1> -->
+   <h1
+   :class="{
+     active: isActive,
+     'font-normal': isNormal,
+     static: true
+   }">
+   Hello world!
+  </h1>
+  <button @click="toggleActive">Toggle Active!</button>
+  <button @click="toggleWeight">Toggle Weight!</button>
+</template>
+
+<style scoped>
+.active {
+  color: red;
+}
+.font-normal {
+  font-weight: 400;
+}
+</style>
