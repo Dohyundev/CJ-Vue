ce4d5b566b97591ef067b5bfd9a88ec3dbf4a577
ParkYoungWoong <thesecon@gmail.com>
2026-08-26 15:51:29 +0900

    260826-15:51

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
-// import { ref } from 'vue'
+import { ref } from 'vue'
 
+const isShow = ref(false)
+
+// 참(Truthy)
+// ...
+
+// 거짓(Falsy)
+// false, 0, null, undefined, NaN, 0n, ''
+// Not a Number
 </script>
 
 <template>
-  <div></div>
+  <button @click="isShow = !isShow">토글</button>
+  <h1 v-if="isShow">배열에 데이터가 들어있어요!</h1>
 </template>
 
 <style scoped></style>
