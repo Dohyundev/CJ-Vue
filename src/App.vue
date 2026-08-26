9667284a12c1c9964723ee8cf117c287f179ee31
ParkYoungWoong <thesecon@gmail.com>
2026-08-26 16:36:28 +0900

    260826-16:36

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,10 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+// import { ref } from 'vue'
 
-const isShow = ref(false)
-
-// 참(Truthy)
-// ...
-
-// 거짓(Falsy)
-// false, 0, null, undefined, NaN, 0n, ''
-// Not a Number
 </script>
 
 <template>
-  <input type="text">
-  <button @click="isShow = !isShow">토글</button>
-  <h1 v-if="isShow">배열에 데이터가 들어있어요!</h1>
+  <div></div>
 </template>
 
 <style scoped></style>
---
+++ src/backups/과일_리스트_렌더링.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+// const fruits = ref(['사과', '바나나', '딸기', '딸기'])
+const fruits = ref([
+  { name: '사과', id: 'skdjfnkw3' },
+  { name: '바나나', id: 'sldkjfn12' },
+  { name: '딸기', id: 'dkj12fn12' },
+  { name: '딸기', id: '017sj1hs2' },
+])
+</script>
+
+<template>
+  <button @click="fruits.unshift({ name: '포도', id: '1231hjsbg' })">
+    추가
+  </button>
+  <ul>
+    <li v-for="(fruit, index) in fruits" :key="fruit.id">
+      {{ index + 1 }}) {{ fruit.name }}
+    </li>
+  </ul>
+</template>
+
+<style scoped></style>
