590310087d406454196e37140a9fb1cc02235657
ParkYoungWoong <thesecon@gmail.com>
2026-08-27 08:36:52 +0900

    260827-08:36

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,48 @@
 <script setup lang="ts">
-// import { ref } from 'vue'
+import { ref } from 'vue'
+import TheLoader from '@/components/TheLoader.vue'
 
+export interface ResponseData {
+  Search: Movie[]
+  totalResults: string
+  Response: string
+}
+export interface Movie {
+  Title: string
+  Year: string
+  imdbID: string
+  Type: string
+  Poster: string
+}
+
+const movies = ref<Movie[]>([])
+const isLoading = ref(true)
+
+async function fetchMovies() {
+  await new Promise(resolve => setTimeout(resolve, 3000))
+  const response = await fetch(`https://omdbapi.com/?apikey=9d38c929&s=spider`)
+  const data = await response.json()
+  movies.value = data.Search
+  isLoading.value = false
+}
+fetchMovies()
 </script>
 
 <template>
-  <div></div>
+  <div>
+    <!-- <h1 v-if="isLoading">로드 중...</h1> -->
+     <TheLoader v-if="isLoading" />
+    <ul @click="console.log(123)">
+      <li v-for="movie in movies" :key="movie.imdbID">
+        {{ movie.Title }} ({{ movie.Year }})
+        <img :src="movie.Poster" />
+      </li>
+    </ul>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+img {
+  width: 100px;
+}
+</style>
