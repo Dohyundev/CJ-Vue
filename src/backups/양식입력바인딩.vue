7e17013c2c03f783344af9063aa990fa122633d4
ParkYoungWoong <thesecon@gmail.com>
2026-08-27 10:14:31 +0900

    260827-10:14

---
+++ src/backups/양식입력바인딩.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const inputText = ref('')
+const className = 'active'
+</script>
+
+<template>
+  <input type="text" :value="inputText" @input="inputText = $event.target.value">
+  <!-- <input type="text" v-model="inputText"> -->
+  <p :class="className">inputText 데이터의 값은: {{ inputText }}</p>
+</template>
+
+<style scoped>
+</style>
