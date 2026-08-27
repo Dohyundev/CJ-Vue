cd0ceb520efd1206a2bfd384cf818ac58f84653e
ParkYoungWoong <thesecon@gmail.com>
2026-08-27 10:50:04 +0900

    260827-10:50

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,10 @@
 <script setup lang="ts">
-import { ref, computed, watch } from 'vue'
+// import { ref } from 'vue'
 
-const count = ref(4)
-const double = computed(function () {
-  return count.value * 2
-}) // 계산된 데이터
-
-// watch(감시할데이터, 실행할함수)
-watch(count, function (newValue, oldValue) {
-  console.log('count 데이터가 변경되었습니다.', newValue, oldValue)
-})
-
-function increase() {
-  count.value += 1
-}
-function decrease() {
-  count.value -= 1
-}
 </script>
 
 <template>
-  <div>{{ count }}</div>
-  <div>{{ double }}</div>
-  <button @click="increase">증가!</button>
-  <button @click="decrease">감소!</button>
+  <div></div>
 </template>
+
+<style scoped></style>
