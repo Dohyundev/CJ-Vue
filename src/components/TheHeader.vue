f4650a8fb2447c33b3f3b0649e13e8f01eab739d
ParkYoungWoong <thesecon@gmail.com>
2026-08-27 17:10:07 +0900

    260827-17:10

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+// import { ref } from 'vue'
+import { RouterLink } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+    <RouterLink to="/movies">Movies</RouterLink>
+  </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  gap: 10px;
+}
+.router-link-exact-active {
+  color: red;
+  font-weight: 700;
+}
+</style>
